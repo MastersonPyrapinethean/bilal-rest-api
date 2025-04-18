38a6669a3587c99e243e8600d2b04c0657047f9c
Harun Biswas Rubel <harunbiswasrubel@gmail.com>
2025-04-19 02:43:54 +0600

    update by Harun

--- src/utils/sendEmail.ts
+++ src/utils/sendEmail.ts
@@ -1,45 +1,45 @@
-import nodemailer from 'nodemailer';
-import dotenv from 'dotenv';
-import path from 'path';
+import dotenv from "dotenv";
+import nodemailer from "nodemailer";
+import path from "path";
 
 // For env File
-dotenv.config({ path: path.resolve(__dirname, '/etc/secrets/.env') });
+dotenv.config({ path: path.resolve(__dirname, "/etc/secrets/.env") });
 
 interface EmailOptions {
-    from: string;
-    to: string;
-    subject: string;
-    text?: string;
-    html?: string;
-    attachments?: { filename: string; content: Buffer }[];
+  from: string;
+  to: string;
+  subject: string;
+  text?: string;
+  html?: string;
+  attachments?: { filename: string; content: Buffer }[];
 }
 
 const sendEmail = async (to: string, pdfBuffer: Buffer) => {
-    const transporter = nodemailer.createTransport({
-        host: process.env.SMTP_HOST,
-        port: 465,
-        secure: true,
-        auth: {
-            user: process.env.SMTP_USER,
-            pass: process.env.SMTP_PASSWORD,
-        },
-    });
+  const transporter = nodemailer.createTransport({
+    host: process.env.SMTP_HOST,
+    port: 465,
+    secure: true,
+    auth: {
+      user: process.env.SMTP_USER,
+      pass: process.env.SMTP_PASSWORD,
+    },
+  });
 
-    const mailOptions: EmailOptions = {
-        from: '"YAAVAAY" <contact@yaavaay.com>', // sender address
-        to: to,
-        subject: 'YAAVAAY - Your Receipt',
-        text: 'Please find attached your receipt.',
-        attachments: [
-            {
-                filename: 'receipt.pdf',
-                content: pdfBuffer,
-            },
-        ],
-    };
+  const mailOptions: EmailOptions = {
+    from: '"YAAVAAY" <contact@yaavaay.com>', // sender address
+    to: "contact@yaavaay.com",
+    subject: "YAAVAAY - Your Receipt",
+    text: "Please find attached your receipt.",
+    attachments: [
+      {
+        filename: "receipt.pdf",
+        content: pdfBuffer,
+      },
+    ],
+  };
 
-    const info = await transporter.sendMail(mailOptions);
-    console.log(`Message sent: ${info.messageId}`);
+  const info = await transporter.sendMail(mailOptions);
+  console.log(`Message sent: ${info.messageId}`);
 };
 
 export default sendEmail;
